4596bf0f1a35cb430b193c3a9f0c0e70ed992400
Munim <munimdevops1111@gmail.com>
2024-03-28 23:45:10 +0300

    Added nginx script and update Vagrant file

---
+++ local_setup/automatic_local_setup/nginx.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Update OS with latest patches
+apt update
+apt upgrade -y
+
+# Install nginx
+apt install nginx -y
+
+# Create Nginx conf file
+echo 'upstream vproapp {
+    server app01:8080;
+}
+
+server {
+    listen 80;
+    location / {
+        proxy_pass http://vproapp;
+    }
+}' > /etc/nginx/sites-available/vproapp
+
+# Remove default nginx conf
+rm -rf /etc/nginx/sites-enabled/default
+
+# Create link to activate website
+ln -s /etc/nginx/sites-available/vproapp /etc/nginx/sites-enabled/vproapp
+
+# Restart Nginx
+systemctl restart nginx
+
+echo "All tasks completed successfully."
